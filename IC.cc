2a9993049e0df525a670fc7ee93fa7acbb9a7f84
vachan-potluri <vachanpotluri1997@gmail.com>
2019-09-25 15:10:19 +0530

    Correct loop through points

--- IC.cc
+++ IC.cc
@@ -6,7 +6,14 @@
 #include "IC.h"
 
 void IC::value_list(const std::vector<Point<2>> &points,
-        std::vector<double> &values, const uint component) const
+        std::vector<double> &values,
+        const uint component) const
 {
-        for(double &cur_value: values) cur_value=0.0; // constant initial condition
+        auto point_iter = points.cbegin();
+        auto val_iter = values.begin();
+        while(point_iter != points.cend()){
+                *val_iter = 0.0; // constant initial condition
+                point_iter++;
+                val_iter++;
+        }
 }
